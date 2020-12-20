faec0374004c84c1e118c2cc3b53a00fe803f6ba
JulioAprígio <julioaprigio8@gmail.com>
2020-12-20 18:49:02 -0300

    adicionando código firebase e removendo temporariamente armazenamento em cartão SD

--- código arduino/detectando_na_agua/detectando_na_agua.ino
+++ código arduino/detectando_na_agua/detectando_na_agua.ino
@@ -1,55 +1,56 @@
-#include<SD.h>
-#include<SPI.h>
 #include <Wire.h>
 #include <DS3231.h>
 
-File myFile;
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
 DS3231 rtc;              //Criação do objeto do tipo DS3231
 RTCDateTime dataehora;   //Criação do objeto do tipo RTCDateTime
+//Pinos do NodeMCU: SDA => D1 SCL => D2
+// Configure com suas credenciais do FireBase
+// Credencial está em configuração do projeto, contas e serviços.
+#define FIREBASE_HOST "detectando-na-agua1-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "a73Mo8zCcRorzwlTdkFuW1YzGnfLVABLphrHkBWf"
+#define WIFI_SSID "nome da sua rede wifi"
+#define WIFI_PASSWORD "senha da sua rede wifi"
 
-int CS = 10; //Pin 10 para UNO
+WiFiServer server(80);// Porta 80 do webserver
 
 void setup() {
   Serial.begin(9600);
   rtc.begin();            //Inicialização do RTC DS3231
   rtc.setDateTime(__DATE__, __TIME__);   //Configurando valores iniciais do RTC DS3231
-  pinMode(CS, OUTPUT); // Declara pino CS/SS como saída
-
-  if (SD.begin(CS)) { // Inicializa o SD Card
-    Serial.println("SD Card pronto para uso."); // Imprime na tela
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("conectando");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
-  else {
-    Serial.println("Falha na inicialização do SD Card.");
-    return;
-  }
-  myFile = SD.open("LAIS.csv", FILE_WRITE); // Cria / Abre arquivo .csv
-
-  if (myFile) { // Se o Arquivo abrir imprime:
-    Serial.println("Escrevendo no Arquivo .csv"); // Imprime na tela
-    myFile.println("Data\tHorario\tcoluna3\tcoluna4\tcoluna5"); // Escreve no Arquivo
-    myFile.println("dd/mm/aaaa\tHH:MM:SS\tunidade3\tunidade4\tunidade5"); // Escreve no Arquivo
-    myFile.close(); // Fecha o Arquivo após escrever
-    Serial.println("Terminado."); // Imprime na tela
-    Serial.println(" ");
-  }
-
-  else {     // Se o Arquivo não abrir
-    Serial.println("Erro ao Abrir Arquivo .csv"); // Imprime na tela
-  }
+  Serial.println();
+  Serial.print("conectado: ");
+  Serial.println(WiFi.localIP());
+  //Iniciar Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void loop() {
   dataehora = rtc.getDateTime();     //Atribuindo valores instantâneos de data e hora à instância dataehora
-  if (myFile = SD.open("LAIS.csv", FILE_WRITE)) {//abre o arquivo para salvar dados
-    myFile.print(dataehora.day);//escreve no arquivo
-    myFile.print("/");//escreve no arquivo
-    myFile.print(dataehora.month);//escreve no arquivo
-    myFile.print("/");//escreve no arquivo
-    myFile.print(dataehora.year);//escreve no arquivo
-    myFile.print("\t");//tabulação
-    myFile.print(dateehora.hour);//escreve no arquivo
-    myFile.print(":");//escreve no arquivo
-    myFile.print(dateehora.minute);//escreve no arquivo
-    myFile.print(":");//escreve no arquivo
-    myFile.print(dateehora.second);//escreve no arquivo
-  }
+  Serial.print(dataehora.year);     //Imprimindo o Ano
+  Serial.print("-");
+  Serial.print(dataehora.month);    //Imprimindo o Mês
+  Serial.print("-");
+  Serial.print(dataehora.day);      //Imprimindo o Dia
+  Serial.print(" ");
+  Serial.print(dataehora.hour);     //Imprimindo a Hora
+  Serial.print(":");
+  Serial.print(dataehora.minute);   //Imprimindo o Minuto
+  Serial.print(":");
+  Serial.println(dataehora.second);   //Imprimindo o Segundo
+  delay(1000);
+  Firebase.pushInt("time/hora",dataehora.hour);
+  Firebase.pushInt("time/minuto",dataehora.minute);
+  Firebase.pushInt("time/segundo",dataehora.second);
+  Firebase.pushInt("time/dia",dataehora.day);
+  Firebase.pushInt("time/mes",dataehora.month);
+  Firebase.pushInt("time/ano",dataehora.year);
+}
