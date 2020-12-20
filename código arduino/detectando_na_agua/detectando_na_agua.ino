466ed3c6c12ed3bae19d7722fceff8156cb77ea7
JulioAprígio <julioaprigio8@gmail.com>
2020-12-20 20:19:36 -0300

    corrigido problema de envio dos dados para o firebase

--- código arduino/detectando_na_agua/detectando_na_agua.ino
+++ código arduino/detectando_na_agua/detectando_na_agua.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include <DS3231.h>
-
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 
@@ -9,10 +8,10 @@ RTCDateTime dataehora;   //Criação do objeto do tipo RTCDateTime
 //Pinos do NodeMCU: SDA => D1 SCL => D2
 // Configure com suas credenciais do FireBase
 // Credencial está em configuração do projeto, contas e serviços.
-#define FIREBASE_HOST "detectando-na-agua1-default-rtdb.firebaseio.com/"
+#define FIREBASE_HOST "detectando-na-agua1-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "a73Mo8zCcRorzwlTdkFuW1YzGnfLVABLphrHkBWf"
-#define WIFI_SSID "nome da sua rede wifi"
-#define WIFI_PASSWORD "senha da sua rede wifi"
+#define WIFI_SSID "nome da sua rede wifi" //nome da sua rede wifi
+#define WIFI_PASSWORD "senha da sua rede wifi" //senha da sua rede wifi
 
 WiFiServer server(80);// Porta 80 do webserver
 
@@ -35,22 +34,21 @@ void setup() {
 
 void loop() {
   dataehora = rtc.getDateTime();     //Atribuindo valores instantâneos de data e hora à instância dataehora
-  Serial.print(dataehora.year);     //Imprimindo o Ano
-  Serial.print("-");
-  Serial.print(dataehora.month);    //Imprimindo o Mês
-  Serial.print("-");
-  Serial.print(dataehora.day);      //Imprimindo o Dia
-  Serial.print(" ");
-  Serial.print(dataehora.hour);     //Imprimindo a Hora
-  Serial.print(":");
-  Serial.print(dataehora.minute);   //Imprimindo o Minuto
-  Serial.print(":");
-  Serial.println(dataehora.second);   //Imprimindo o Segundo
-  delay(1000);
-  Firebase.pushInt("time/hora",dataehora.hour);
-  Firebase.pushInt("time/minuto",dataehora.minute);
-  Firebase.pushInt("time/segundo",dataehora.second);
-  Firebase.pushInt("time/dia",dataehora.day);
-  Firebase.pushInt("time/mes",dataehora.month);
-  Firebase.pushInt("time/ano",dataehora.year);
+//  Serial.print(dataehora.year);     //Imprimindo o Ano
+//  Serial.print("-");
+//  Serial.print(dataehora.month);    //Imprimindo o Mês
+//  Serial.print("-");
+//  Serial.print(dataehora.day);      //Imprimindo o Dia
+//  Serial.print(" ");
+//  Serial.print(dataehora.hour);     //Imprimindo a Hora
+//  Serial.print(":");
+//  Serial.print(dataehora.minute);   //Imprimindo o Minuto
+//  Serial.print(":");
+//  Serial.println(dataehora.second);   //Imprimindo o Segundo
+  Firebase.setInt("time/hora",dataehora.hour);
+  Firebase.setInt("time/minuto",dataehora.minute);
+  Firebase.setInt("time/segundo",dataehora.second);
+  Firebase.setInt("time/dia",dataehora.day);
+  Firebase.setInt("time/mes",dataehora.month);
+  Firebase.setInt("time/ano",dataehora.year);
 }
